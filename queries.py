da76210819eb00175cf5baab35fbaf5a96e8c843
Dima K <@users.noreply.github.com>
2024-07-12 12:28:41 +0200

    Solved Window funtions challenge

--- queries.py
+++ queries.py
@@ -1,8 +1,53 @@
 # pylint:disable=C0111,C0103
 
+import sqlite3
+
+
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = """
+    SELECT
+	Orders.OrderID,
+	Orders.CustomerID,
+	Orders.OrderDate,
+        RANK () OVER (
+	    PARTITION BY Orders.CustomerID
+	    ORDER BY Orders.OrderDate
+    ) AS order_rank
+    FROM Orders
+    """
+    conn = sqlite3.connect('data/ecommerce.sqlite')
+    db = conn.cursor()
+    db.execute(query)
+    results = db.fetchall()
+    print(results)
+    return results
 
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = """
+    WITH output_ AS (
+    SELECT
+	    Orders.OrderID,
+	    Orders.CustomerID,
+	    Orders.OrderDate,
+    SUM(OrderDetails.UnitPrice * OrderDetails.Quantity) OVER (
+	    PARTITION BY Orders.CustomerID
+	    Order BY Orders.OrderDate) AS  OrderCumulativeAmount
+    FROM
+	    Orders
+    JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+    )
+
+    SELECT DISTINCT output_.OrderID,
+	    output_.CustomerID,
+	    output_.OrderDate,
+	    output_.OrderCumulativeAmount
+    FROM output_
+
+    """
+    conn = sqlite3.connect('data/ecommerce.sqlite')
+    db = conn.cursor()
+    db.execute(query)
+    results = db.fetchall()
+    print(results)
+    return results
